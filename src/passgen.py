afc5d66246ba0907bf38261fbfa759c1a00a035e
Animesh Sharma <animesharma08+github@gmail.com>
2023-08-18 12:47:30 -0400

    Added skeleton code for passgen.py

---
+++ src/passgen.py
@@ -0,0 +1,44 @@
+import json
+from argparse import ArgumentParser
+
+class ParseArguments:
+    """
+    
+    """
+    def __init__(self, parser: ArgumentParser) -> None:
+        """
+        
+        """
+
+    def validate_args(self, args) -> None:
+        """
+        
+        """
+
+    def define_args(self) -> None:
+        """
+        
+        """
+
+class AcronymPassphraseGenerator:
+    """
+    
+    """
+
+    def __init__(self) -> None:
+        """
+        
+        """
+        pass
+    
+    
+
+    def generate_passphrase(self) -> str:
+        """
+        
+        """
+
+
+if __name__ == "__main__":
+    ParseArguments(parser=ArgumentParser())
+    AcronymPassphraseGenerator.generate_passphrase()
